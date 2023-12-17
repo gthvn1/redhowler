07f9634ade3dc58d37bebf679c670012e45753b4
Guillaume <guillaume.thouvenin@vates.tech>
2023-12-17 12:30:02 +0100

    implement parsing of let statement

--- src/ast.rs
+++ src/ast.rs
@@ -7,29 +7,46 @@ pub trait Node {
     fn token_literal(&self) -> String;
 }
 
+// Statement does not produce value.
 pub trait Statement: Node {
     // This dummy method is used for debugging.
     fn statement_node(&self) {}
 }
 
+// Expression produces value.
 pub trait Expression: Node {
     // This dummy method is used for debugging.
     fn expression_node(&self) {}
 }
 
+// ============================================================================
+// PROGRAM
+// ============================================================================
 // This is the root of our AST.
-struct Program {
+#[allow(dead_code)]
+pub struct Program {
     // 1. As we are using a trait as a type we need to use dynamic dispatch to
     // allow compiler to decide at runtime which type to use.
     // 2. Size of Statement is not known at compile time because different types
     // can implement the Statement. To solve that we can use Box smartpointer
     // that allocates the data on the Heap. So know the size is the size of the
     // smart pointer and it is known at compile time.
-    statements: Vec<Box<dyn Statement>>,
+    pub statements: Vec<Box<dyn Statement>>,
 }
 
+#[allow(dead_code)]
 impl Program {
-    fn token_literal(&self) -> String {
+    pub fn new() -> Self {
+        Program {
+            statements: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, stmt: Box<dyn Statement>) {
+        self.statements.push(stmt);
+    }
+
+    pub fn token_literal(&self) -> String {
         if self.statements.len() > 0 {
             self.statements[0].token_literal()
         } else {
@@ -38,15 +55,49 @@ impl Program {
     }
 }
 
-// LetStatement is a statement that binds a value to a name.
+// ============================================================================
+// LET STATEMENT
+// ============================================================================
+// LetStatement binds a value to a name.
 // Let's have a look to `let x = 5 * 5;`
 // - We need a node for the token `let`.
 // - We need a node for the variable name `x`.
 // - We need a node for the expression that produces the value.
-struct LetStatement {
-    token: Token,               // The token.LET token.
-    name: Identifier,           // Node for the variable name.
-    value: Box<dyn Expression>, // Node for the value expression.
+
+#[allow(dead_code)]
+pub struct LetStatementBuilder {
+    token: Token,
+    name: Option<Identifier>,
+    //value: Option<Box<dyn Expression>>,
+}
+
+#[allow(dead_code)]
+impl LetStatementBuilder {
+    pub fn new(token: &Token) -> Self {
+        LetStatementBuilder {
+            token: token.clone(),
+            name: None,
+            //value: None,
+        }
+    }
+
+    pub fn name(&mut self, name: Identifier) {
+        self.name = Some(name);
+    }
+
+    pub fn build(self) -> LetStatement {
+        LetStatement {
+            token: self.token,
+            name: self.name.unwrap(),
+            //value: self.value.unwrap(),
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub struct LetStatement {
+    token: Token, // The token.LET token.
+    name: Identifier,
 }
 
 impl Node for LetStatement {
@@ -59,8 +110,12 @@ impl Statement for LetStatement {
     fn statement_node(&self) {}
 }
 
+// ============================================================================
+// IDENTIFIER
+// ============================================================================
 // Identifier is a node that holds the name of the variable.
-struct Identifier {
+#[allow(dead_code)]
+pub struct Identifier {
     token: Token,  // The token.IDENT token.
     value: String, // The value of the identifier.
 }
@@ -72,4 +127,14 @@ impl Node for Identifier {
 }
 impl Expression for Identifier {
     fn expression_node(&self) {}
+}
+
+#[allow(dead_code)]
+impl Identifier {
+    pub fn new(token: &Token) -> Self {
+        Identifier {
+            token: token.clone(),
+            value: token.literal(),
+        }
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -3,3 +3,132 @@
 // in the process.
 // We are constructing a recursive descent parser, which is a type of top-down
 // parsing.
+use crate::ast::{self, Node};
+use crate::lexer::Lexer;
+use crate::token::{Token, TokenType};
+
+#[allow(dead_code)]
+struct Parser<'l> {
+    lexer: Lexer<'l>,
+    cur_token: Token,
+    peek_token: Token,
+}
+
+#[allow(dead_code)]
+impl<'l> Parser<'l> {
+    fn new(lexer: Lexer<'l>) -> Self {
+        let mut p = Parser {
+            lexer,
+            cur_token: Token {
+                token_type: TokenType::Illegal,
+                literal: String::from("Dummy"),
+            },
+            peek_token: Token {
+                token_type: TokenType::Illegal,
+                literal: String::from("Dummy"),
+            },
+        };
+
+        // Read two tokens, so cur_token and peek_token are both set.
+        p.next_token();
+        p.next_token();
+        p
+    }
+
+    fn next_token(&mut self) {
+        self.cur_token = self.peek_token.clone();
+        self.peek_token = self.lexer.next_token();
+    }
+
+    fn parse_program(&mut self) -> ast::Program {
+        let mut program = ast::Program::new();
+
+        while self.cur_token.token_type != TokenType::EOF {
+            let stmt_opt = self.parse_statement();
+            if let Some(stmt) = stmt_opt {
+                program.push(stmt);
+            }
+            self.next_token();
+        }
+        program
+    }
+
+    fn parse_statement(&mut self) -> Option<Box<dyn ast::Statement>> {
+        match self.cur_token.token_type {
+            TokenType::Let => {
+                eprintln!("Parsing let statement");
+                self.parse_let_statement()
+            }
+            _ => None,
+        }
+    }
+
+    fn parse_let_statement(&mut self) -> Option<Box<dyn ast::Statement>> {
+        let mut stmt_builder = ast::LetStatementBuilder::new(&self.cur_token);
+
+        if !self.expect_peek(TokenType::Ident) {
+            eprintln!("Expected identifier, got {}", self.peek_token.literal());
+            return None;
+        }
+
+        stmt_builder.name(ast::Identifier::new(&self.cur_token));
+
+        if !self.expect_peek(TokenType::Assign) {
+            eprintln!("Expected assign, got {}", self.peek_token.literal());
+            return None;
+        }
+
+        // TODO: We're skipping the expressions until we encounter a semicolon.
+        while !self.cur_token_is(TokenType::Semicolon) {
+            self.next_token();
+        }
+
+        let stmt = stmt_builder.build();
+        eprintln!("Let statement: {}", stmt.token_literal());
+        Some(Box::new(stmt))
+    }
+
+    fn cur_token_is(&self, token_type: TokenType) -> bool {
+        self.cur_token.token_type == token_type
+    }
+
+    fn peek_token_is(&self, token_type: TokenType) -> bool {
+        self.peek_token.token_type == token_type
+    }
+
+    // If the next token is of expected type then we advance to next token
+    // and return true, otherwise we return false.
+    fn expect_peek(&mut self, token_type: TokenType) -> bool {
+        if self.peek_token_is(token_type) {
+            self.next_token();
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_let_statements() {
+        let input = "
+            let x = 5;
+            let y = 10;
+            let foobar = 838383;
+        ";
+
+        let l = Lexer::new(input);
+        let mut p = Parser::new(l);
+
+        let program = p.parse_program();
+
+        program.statements.iter().for_each(|stmt| {
+            eprintln!("{}", stmt.token_literal());
+        });
+
+        assert_eq!(program.statements.len(), 3);
+    }
+}
