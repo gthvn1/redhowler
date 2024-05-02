b24a1c3775554d2c45c33c49ac839077aa5182eb
Guillaume <guillaume.thouvenin@vates.tech>
2024-05-02 11:25:40 +0200

    update a comment

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use redhowler::repl;
 fn main() {
     println!("Welcome to Monkey Islang!!!");
     println!("This is the REPL for Monkey programming language.");
-    println!("Feel free to type commands or 'q;'");
+    println!("Feel free to type some code or 'q;'");
 
     repl::start();
 }
